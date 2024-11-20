ef16f0dd1d280b5e76ed9646a1969dd9a73ad896
Blackreaper9 <utm23090485@utma.edu.mx>
2024-11-19 23:26:22 -0600

    3 Modals

---
+++ qutmae/api/src/models/QuestionsModel.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+interface IQuestion{
+    title:String
+    type: " radio" | "checkbox" | "select"| " text"
+    isMandatory: boolean,
+    questionaireId:Schema.Types.ObjectId | string;
+   
+}
+
+const QuestionSchema = new Schema<IQuestion>({
+    title:{
+        type:String,
+        required:true
+    },
+    type:{
+        type:String,
+        enum:[" radio" , "checkbox" , "select", " text"],
+        required:true
+    },
+    isMandatory:{
+        type:Boolean,
+        required:true
+    },
+    questionaireId:{
+        type: Schema.Types.ObjectId,
+        ref: "questionnaires",
+        required:true
+    }
+})
+
+export const QuestionModel = model("questions", QuestionSchema);
